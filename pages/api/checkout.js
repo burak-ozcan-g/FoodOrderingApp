bbeaabdad87df7fd414aba1c6ed50c4c2858842a
burakozcan <burak.ozcan.g@hotmail.com>
2023-06-11 17:39:07 +0300

    vercel 2

--- pages/api/checkout.js
+++ pages/api/checkout.js
@@ -10,8 +10,8 @@ export default async function handler(req, res) {
       const session = await stripe.checkout.sessions.create({
         line_items: req.body.lineItems,
         mode: 'payment',
-        success_url: `https://food-ordering-app-l4eo.vercel.app/success`,
-        cancel_url: `https://food-ordering-app-l4eo.vercel.app`,
+        success_url: `https://food-ordering-app-fawn.vercel.app/success`,
+        cancel_url: `https://food-ordering-app-fawn.vercel.app`,
       });
       res.redirect(303, session.url);
     } catch (err) {
--- pages/orders/[id].jsx
+++ pages/orders/[id].jsx
@@ -95,7 +95,7 @@ const Order = ({order}) => {
 
 
 export const getServerSideProps = async ({ params }) => {
-  const res = await axios.get(`https://food-ordering-app-l4eo.vercel.app/api/orders/${params.id}`)
+  const res = await axios.get(`https://food-ordering-app-fawn.vercel.app/api/orders/${params.id}`)
   return {
     props: {
       order: res.data,
