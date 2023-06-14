b1bc4e0b4c1c2afd9ad2db1e074925992146111c
burakozcan <burak.ozcan.g@hotmail.com>
2023-06-15 01:32:48 +0300

    nextconfig

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = {
     return [
       {
         // matching all API routes
-        source: "/api/checkout",
+        source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
