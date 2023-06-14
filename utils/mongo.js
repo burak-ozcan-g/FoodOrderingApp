e7c662a3f943747d3fb5cb663a98234462c4b1cf
burakozcan <burak.ozcan.g@hotmail.com>
2023-06-14 13:53:28 +0300

    mongodburi

--- utils/mongo.js
+++ utils/mongo.js
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URL = process.env.MONGODB_URL
+const MONGODB_URI = process.env.MONGODB_URI
 
-if (!MONGODB_URL) {
-    throw new Error('Invalid/Missing environment variable: "MONGODB_URL"');
+if (!MONGODB_URI) {
+    throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
 }
 
 let cached = global.mongoose
@@ -22,7 +22,7 @@ async function dbConnect() {
             bufferCommands: false,
         }
 
-        cached.promise = mongoose.connect(MONGODB_URL, opts).then((mongoose) => {
+        cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
             return mongoose
         })
     }
